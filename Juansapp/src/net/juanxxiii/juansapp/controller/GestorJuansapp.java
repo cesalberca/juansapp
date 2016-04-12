557281b6f1becb55ed7198a5def1cd0491d7e90d
cesar <cesalberca@hotmail.es>
2016-04-12 16:47:00 +0200

    Gestor que se encargará de crear y actualizar la bbdd

---
+++ Juansapp/src/net/juanxxiii/juansapp/controller/GestorJuansapp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.juanxxiii.juansapp.controller;
+
+/**
+ *
+ * @author dam
+ */
+public class GestorJuansapp {
+    private User us;
+    private Message ms;
+    
+    public void addUser() {
+        
+    }
+    
+}
